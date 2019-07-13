31926148532662b7981ecb83dc29d7a80066e08f
Conrad Taylor <conradwt@gmail.com>
2019-07-13 04:27:49 -0700

    Upgrade to Elixir 1.9.0 and refresh packages to latest stable release

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ZeroPhoenix.Mixfile do
     [
       app: :zero_phoenix,
       version: "0.0.1",
-      elixir: "~> 1.8.1",
+      elixir: "~> 1.9.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
