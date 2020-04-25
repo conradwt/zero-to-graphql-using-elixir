a5daf26c2ff3c99c9a79377eac6c091ee15bcb55
Conrad Taylor <conradwt@gmail.com>
2020-04-25 05:11:44 -0700

    Upgrade files to match Phoenix 1.5 generated application.

--- lib/zero_phoenix_web/endpoint.ex
+++ lib/zero_phoenix_web/endpoint.ex
@@ -1,11 +1,20 @@
 defmodule ZeroPhoenixWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :zero_phoenix
 
+  # The session will be stored in the cookie and signed,
+  # this means its contents can be read but not tampered with.
+  # Set :encryption_salt if you would also like to encrypt it.
+  @session_options [
+    store: :cookie,
+    key: "_zero_phoenix_key",
+    signing_salt: "JuFfVLQs"
+  ]
+
   socket "/socket", ZeroPhoenixWeb.UserSocket,
     websocket: true,
     longpoll: false
 
-  socket "/live", Phoenix.LiveView.Socket
+  socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
 
   # Serve at "/" the static files from "priv/static" directory.
   #
@@ -23,10 +32,15 @@ defmodule ZeroPhoenixWeb.Endpoint do
     socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
     plug Phoenix.LiveReloader
     plug Phoenix.CodeReloader
+    plug Phoenix.Ecto.CheckRepoStatus, otp_app: :zero_phoenix
   end
 
+  plug Phoenix.LiveDashboard.RequestLogger,
+    param_key: "request_logger",
+    cookie_key: "request_logger"
+
   plug Plug.RequestId
-  plug Plug.Logger
+  plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
 
   plug Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
@@ -35,14 +49,6 @@ defmodule ZeroPhoenixWeb.Endpoint do
 
   plug Plug.MethodOverride
   plug Plug.Head
-
-  # The session will be stored in the cookie and signed,
-  # this means its contents can be read but not tampered with.
-  # Set :encryption_salt if you would also like to encrypt it.
-  plug Plug.Session,
-    store: :cookie,
-    key: "_task_tester_key",
-    signing_salt: "JuFfVLQs"
-
+  plug Plug.Session, @session_options
   plug ZeroPhoenixWeb.Router
 end
