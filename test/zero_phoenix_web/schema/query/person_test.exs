d86b54dc1edaf34c454f8c60acb16303803d9c8f
Conrad Taylor <conradwt@gmail.com>
2021-04-17 19:55:26 -0700

    Add test for the person query.

---
+++ test/zero_phoenix_web/schema/query/person_test.exs
@@ -0,0 +1,43 @@
+defmodule ZeroPhoenixWeb.Schema.Query.PersonTest do
+  use ZeroPhoenixWeb.ConnCase, async: true
+
+  import Ecto.Query, only: [first: 1]
+
+  alias ZeroPhoenix.Account.Person
+  alias ZeroPhoenix.Repo
+
+  setup do
+    ZeroPhoenix.Seeds.run()
+  end
+
+  @query """
+  query GetPerson($personId: ID!) {
+    person(id: $personId) {
+      email
+    }
+  }
+  """
+
+  test "get person by ID" do
+    person =
+      Person
+      |> first()
+      |> Repo.one()
+
+    response =
+      post(
+        build_conn(),
+        "/api",
+        query: @query,
+        variables: %{"personId" => person.id}
+      )
+
+    assert json_response(response, 200) == %{
+             "data" => %{
+               "person" => %{
+                 "email" => "conradwt@gmail.com"
+               }
+             }
+           }
+  end
+end
