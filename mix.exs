9364b9ee6c79fc353a0207721b57a239edd029fb
Conrad Taylor <conradwt@gmail.com>
2021-04-17 19:51:39 -0700

    Add the dev/support directory.

--- mix.exs
+++ mix.exs
@@ -25,7 +25,8 @@ defmodule ZeroPhoenix.Mixfile do
   end
 
   # Specifies which paths to compile per environment.
-  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(:test), do: ["lib", "dev/support", "test/support"]
+  defp elixirc_paths(:dev), do: ["lib", "dev/support"]
   defp elixirc_paths(_), do: ["lib"]
 
   # Specifies your project dependencies.
