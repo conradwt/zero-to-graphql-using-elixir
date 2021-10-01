9f8375cf7a5f02149be6b6e1bafa19e21859d82c
Conrad Taylor <conradwt@gmail.com>
2021-09-30 22:52:41 -0700

    Upgrade to Phoenix 1.5.13.

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule ZeroPhoenix.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.12"},
+      {:phoenix, "~> 1.5.13"},
       {:phoenix_ecto, "~> 4.2.1"},
       {:ecto_sql, "~> 3.6.1"},
       {:postgrex, "~> 0.15.9"},
