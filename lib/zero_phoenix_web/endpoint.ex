2f75d7a9b8f1c9f23bd331b669a622751c9f7a08
Conrad Taylor <conradwt@gmail.com>
2021-11-28 17:43:27 -0800

    Upgrade to Phoenix 1.6.2.

--- lib/zero_phoenix_web/endpoint.ex
+++ lib/zero_phoenix_web/endpoint.ex
@@ -7,55 +7,47 @@ defmodule ZeroPhoenixWeb.Endpoint do
   @session_options [
     store: :cookie,
     key: "_zero_phoenix_key",
-    signing_salt: "JuFfVLQs"
+    signing_salt: "pEK+TkWC"
   ]
 
-  socket("/socket", ZeroPhoenixWeb.UserSocket,
-    websocket: true,
-    longpoll: false
-  )
-
-  socket("/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]])
+  socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
 
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
   # when deploying your static files in production.
-  plug(Plug.Static,
+  plug Plug.Static,
     at: "/",
     from: :zero_phoenix,
     gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
-  )
+    only: ~w(assets fonts images favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
-    socket("/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket)
-    plug(Phoenix.LiveReloader)
-    plug(Phoenix.CodeReloader)
-    plug(Phoenix.Ecto.CheckRepoStatus, otp_app: :zero_phoenix)
+    socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
+    plug Phoenix.LiveReloader
+    plug Phoenix.CodeReloader
+    plug Phoenix.Ecto.CheckRepoStatus, otp_app: :zero_phoenix
   end
 
-  plug(Phoenix.LiveDashboard.RequestLogger,
+  plug Phoenix.LiveDashboard.RequestLogger,
     param_key: "request_logger",
     cookie_key: "request_logger"
-  )
 
-  plug(Plug.RequestId)
-  plug(Plug.Telemetry, event_prefix: [:phoenix, :endpoint])
+  plug Plug.RequestId
+  plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
 
-  plug(Plug.Parsers,
+  plug Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json, Absinthe.Plug.Parser],
     pass: ["*/*"],
     json_decoder: Phoenix.json_library()
-  )
 
-  plug(Plug.MethodOverride)
-  plug(Plug.Head)
-  plug(Plug.Session, @session_options)
+  plug Plug.MethodOverride
+  plug Plug.Head
+  plug Plug.Session, @session_options
 
   plug CORSPlug, origin: ["*"]
 
-  plug(ZeroPhoenixWeb.Router)
+  plug ZeroPhoenixWeb.Router
 end
