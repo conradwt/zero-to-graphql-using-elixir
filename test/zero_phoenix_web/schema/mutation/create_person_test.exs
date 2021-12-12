a908b6f1fe1369e98a7f5b602ed8e4d8c8cfea48
Conrad Taylor <conradwt@gmail.com>
2021-12-11 23:06:08 -0800

    WIP: add create person mutation.

---
+++ test/zero_phoenix_web/schema/mutation/create_person_test.exs
@@ -0,0 +1,51 @@
+defmodule ZeroPhoenixWeb.Schema.Query.CreatePersonTest do
+  use ZeroPhoenixWeb.ConnCase, async: true
+
+  import Ecto.Query, only: [first: 1]
+
+  alias ZeroPhoenix.Account.Person
+  alias ZeroPhoenix.Repo
+
+  setup do
+    ZeroPhoenix.Seeds.run()
+  end
+
+  test "create person" do
+    query = """
+      mutation CreatePerson($person: Person!) {
+        createPerson(person: $person) {
+          firstName
+          lastName
+          email
+          userName
+        }
+      }
+    """
+
+    variables = %{
+        "firstName" => "Jane",
+        "lastName" => "Doe",
+        "email" => "jane@example.com",
+        "username" => "janed"
+      }
+
+    response =
+      post(
+        build_conn(),
+        "/graphql",
+        query: query,
+        variables: variables
+      )
+
+    assert json_response(response, 200) == %{
+      "data" => %{
+        "createPerson" => %{
+          "firstName" => "Jane",
+          "lastName" => "Doe",
+          "email" => "jane@example.com",
+          "username" => "janed"
+        }
+      }
+    }
+  end
+end
