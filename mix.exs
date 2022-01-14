621cce7ba9976149884d43c010a9b8f436f871d4
Conrad Taylor <conradwt@gmail.com>
2022-01-14 01:20:07 -0800

    Upgrade-to-elixir-1.13.2-otp-24 (#60)

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ZeroPhoenix.Mixfile do
     [
       app: :zero_phoenix,
       version: "3.3.4",
-      elixir: "~> 1.13.1",
+      elixir: "~> 1.13.2",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
