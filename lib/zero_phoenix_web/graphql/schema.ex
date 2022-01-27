328dcb6d0cb78766039b73eaa1d026458c9f37f7
Conrad Taylor <conradwt@gmail.com>
2022-01-26 22:58:49 -0800

    WIP: add missing files.

--- lib/zero_phoenix_web/graphql/schema.ex
+++ lib/zero_phoenix_web/graphql/schema.ex
@@ -1,41 +1,15 @@
 defmodule ZeroPhoenixWeb.Graphql.Schema do
   use Absinthe.Schema
 
-  import_types(ZeroPhoenixWeb.Graphql.Types.{Person, PersonInput})
-
-  alias ZeroPhoenix.Account.Person
-  alias ZeroPhoenix.Account
-  alias ZeroPhoenixWeb.Graphql.Resolvers
+  import_types(ZeroPhoenixWeb.Graphql.Types.Person)
+  import_types(ZeroPhoenixWeb.Graphql.Schemas.Queries.Person)
+  import_types(ZeroPhoenixWeb.Graphql.Schemas.Mutations.Person)
 
   query do
-    field :person, type: :person do
-      arg(:id, non_null(:id))
-
-      resolve(fn %{id: id}, _info ->
-        case Account.get_person(id) do
-          %Person{} = person ->
-            {:ok, person}
-
-          _error ->
-            {:error, "Person id #{id} not found"}
-        end
-      end)
-    end
-
-    field :people, type: list_of(:person) do
-      arg(:ids, list_of(:id), default_value: [])
-
-      resolve(fn %{ids: ids}, _info ->
-        {:ok, Account.get_people(ids)}
-      end)
-    end
+    import_types(:person_queries)
   end
 
   mutation do
-    field :create_person, type: :person do
-      arg :input, non_null(:person_input)
-
-      resolve &Resolvers.Person.create_person/3
-    end
+    import_types(:person_mutations)
   end
 end
--- lib/zero_phoenix_web/graphql/types/person.ex
+++ lib/zero_phoenix_web/graphql/types/person.ex
@@ -29,4 +29,19 @@ defmodule ZeroPhoenixWeb.Graphql.Types.Person do
       end)
     end
   end
+
+  @desc "a person input"
+  input_object :person_input do
+    @desc "first name of a person"
+    field(:first_name, non_null(:string))
+
+    @desc "last name of a person"
+    field(:last_name, non_null(:string))
+
+    @desc "username of a person"
+    field(:username, non_null(:string))
+
+    @desc "email of a person"
+    field(:email, non_null(:string))
+  end
 end
