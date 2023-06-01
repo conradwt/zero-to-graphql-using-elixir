c718c66bbd5a274af3ceb37566f55d4df89dc049
Conrad Taylor <conradwt@gmail.com>
2023-05-31 23:10:42 -0700

    Update to Phoenix 1.7.3. (#154)

--- mix.exs
+++ mix.exs
@@ -36,7 +36,7 @@ defmodule ZeroPhoenix.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.7.2"},
+      {:phoenix, "~> 1.7.3"},
       {:phoenix_ecto, "~> 4.4.1"},
       {:ecto_sql, "~> 3.10.1"},
       {:postgrex, "~> 0.17.1"},
