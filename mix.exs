d5541fd4862a51b7f7a4d5a44e3dd7f522e8018a
Conrad Taylor <conradwt@gmail.com>
2023-07-20 00:34:42 -0700

    Update to Elixir 1.15.4 and refresh Hex packages.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ZeroPhoenix.Mixfile do
     [
       app: :zero_phoenix,
       version: "3.5.3",
-      elixir: "~> 1.15.3",
+      elixir: "~> 1.15.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
