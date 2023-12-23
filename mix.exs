9f07031245327806f314bb50b4030a51a1f722e5
Conrad Taylor <conradwt@gmail.com>
2023-12-23 00:06:03 -0800

    Update app version.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ZeroPhoenix.Mixfile do
   def project do
     [
       app: :zero_phoenix,
-      version: "3.5.3",
+      version: "3.6.0",
       elixir: "~> 1.16.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
