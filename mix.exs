4cb0427719e5e5261c4e871edd59a23129076960
Conrad Taylor <conradwt@gmail.com>
2024-05-26 19:32:35 -0700

    Update to Erlang 27.0 and Elixir 1.17.0-rc.0.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ZeroPhoenix.Mixfile do
     [
       app: :zero_phoenix,
       version: "3.6.0",
-      elixir: "~> 1.16.3",
+      elixir: "~> 1.17.0-rc.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
