2cfa0492b0178ab294779635fcd5f9ee17e8c072
Conrad Taylor <conradwt@gmail.com>
2026-01-09 23:45:41 -0800

    Update to Elixir 1.19.5. (#296)

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ZeroPhoenix.Mixfile do
     [
       app: :zero_phoenix,
       version: "3.6.0",
-      elixir: "~> 1.19.4",
+      elixir: "~> 1.19.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
